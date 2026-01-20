b3961df5a668b8eda4e18bfca498c8c846babff5
Joanna Kraciuk <j.kraciuktest@gmail.com>
2026-01-20 13:55:08 +0100

    Refaktor kodu, dodanie base_page oraz dziedziczenia w login_page

---
+++ pages/base_page.py
@@ -0,0 +1,103 @@
+from typing import Optional, Union
+from pathlib import Path
+from playwright.sync_api import Page, Locator, expect
+
+
+class BasePage:
+    BASE_URL = "https://dotesthere.com/"
+
+    def __init__(self, page: Page, default_timeout_ms: int = 5000):
+        self.page = page
+        self.page.set_default_timeout(default_timeout_ms)
+
+
+    def open(self, url: str = BASE_URL):
+        self.page.goto(url)
+        return url
+
+
+    def wait_for_url(self, url_or_regex: Union[str, object], timeout_ms: int = 5000):
+        expect(self.page).to_have_url(url_or_regex, timeout=timeout_ms)
+
+    def wait_visible(self, locator: Locator, timeout_ms: int = 5000):
+        expect(locator).to_be_visible(timeout=timeout_ms)
+        return locator
+
+    def wait_hidden(self, locator: Locator, timeout_ms: int = 5000):
+        expect(locator).to_be_hidden(timeout=timeout_ms)
+        return locator
+
+    def wait_enabled(self, locator: Locator) -> bool:
+        expect(locator).to_be_enabled()
+        return True
+
+    def wait_disabled(self, locator: Locator) -> bool:
+        expect(locator).to_be_disabled()
+        return True
+
+    def wait_text(self, locator: Locator, text: str):
+        expect(locator).to_have_text(text)
+
+    def get_input_value(self, locator: Locator) -> str:
+        return locator.input_value()
+
+    def wait_for_spinner(self, spinner: Locator, timeout_ms: int = 10000):
+        try:
+            expect(spinner).to_be_visible(timeout=2000)
+        except Exception:
+            pass
+        expect(spinner).to_be_hidden(timeout=timeout_ms)
+
+    def click(self, locator: Locator, timeout_ms: int = 5000):
+        self.wait_visible(locator, timeout_ms)
+        locator.click()
+        return locator
+
+    def fill(self, locator: Locator, text: str, timeout_ms: int = 5000, clear: bool = True):
+        self.wait_visible(locator, timeout_ms)
+        if clear:
+            locator.fill("")
+        locator.fill(text)
+        return locator
+
+    def select_option_in_dropdown(self, locator: Locator, label: str):
+        locator.select_option(label=label)
+
+    def resolve_file_path(self, relative_path: str) -> str:
+        return str((Path.cwd() / relative_path).resolve())
+
+    def set_file(self, locator: Locator, relative_path: str):
+        file_path = str((Path.cwd() / relative_path).resolve())
+        locator.set_input_files(file_path)
+
+    def click_and_wait_for(self, button: Locator, element_to_wait: Locator):
+        button.click()
+        expect(element_to_wait).to_be_visible()
+
+    def get_text(self, locator: Locator) -> str:
+        return locator.inner_text()
+
+    def get_input_value(self, locator: Locator) -> str:
+        return locator.input_value()
+
+    def expect_value(self, locator: Locator, expected: str):
+        expect(locator).to_have_value(expected)
+
+    def get_visible_texts(self, locator: Locator) -> list[str]:
+        values = []
+        count = locator.count()
+
+        for i in range(count):
+            element = locator.nth(i)
+            if element.is_visible():
+                text = element.text_content()
+                if text:
+                    values.append(text.strip())
+        return values
+
+
+
+
+
+
+
--- pages/login_page.py
+++ pages/login_page.py
@@ -1,11 +1,11 @@
 from pathlib import Path
-
+from pages.base_page import BasePage
 from playwright.sync_api import Page, Locator, expect
 
-class LoginPage:
-    BASE_URL = "https://dotesthere.com/"
+class LoginPage(BasePage):
 
     def __init__(self, page: Page):
+        super().__init__(page)
         self.page: Page = page
         self.username_input: Locator = page.locator("#username")
         self.password_input: Locator = page.locator("#password")
@@ -30,72 +30,64 @@ class LoginPage:
 
         self.last_name_cells: Locator = page.locator('#table1 tbody tr')
 
-    def open(self) -> str:
-        self.page.goto(self.BASE_URL)
-        return self.page.url
+    def open_login_page(self):
+        return self.open(self.BASE_URL)
 
     def login(self, username: str, password: str) -> str:
-        self.username_input.fill(username)
-        self.password_input.fill(password)
-        self.login_button.click()
+        self.fill(self.username_input, username)
+        self.fill(self.password_input, password)
+        self.click(self.login_button)
         expect(self.flash_message).to_be_visible()
         return self.flash_message.inner_text()
 
-    def select_option(self, label: str = "Option 2") -> str:
-        self.dropdown.select_option(label=label)
+    def select_option(self, label: str = 'Option 2') -> str:
+        self.select_option_in_dropdown(self.dropdown, label)
         expect(self.toast).to_be_visible()
         return self.toast.inner_text()
 
-    def upload_file(self, relative_path: str) -> str:
-        file_path = str((Path.cwd() / relative_path).resolve())
-        self.file_input.set_input_files(file_path)
-        self.upload_button.click()
-        expect(self.upload_result).to_be_visible()
+    def upload_file(self, relative_path: str = 'tests/files/Plik 8.pdf') -> str:
+        self.set_file(self.file_input, relative_path)
+        self.click_and_wait_for(self.upload_button, self.upload_result)
         return self.upload_result.inner_text()
 
     def get_flash_message_text(self) -> str:
         expect(self.flash_message).to_be_visible()
         return self.flash_message.inner_text()
 
-    def add_element(self):
-        self.add_btn.click()
-        confirmation_message = self.toast.inner_text()
-        return confirmation_message
+    def add_element(self) -> str:
+        self.click(self.add_btn)
+        self.wait_visible(self.toast)  # <- kluczowy krok, anty-flaky
+        return self.get_text(self.toast)
 
     def delete_element(self):
-        self.add_btn.click()
-        self.delete_btn.click()
-        confirmation_message = self.toast.inner_text()
-        return confirmation_message
+        self.click(self.add_btn)
+        self.click(self.delete_btn)
+        self.wait_visible(self.toast)
+        return self.get_text(self.toast)
 
-    def enable_input(self):
-        self.enable_btn.click()
-        return self.dynamic_input.is_enabled()
+    def enable_input(self) -> bool:
+        self.click(self.enable_btn)
+        return self.wait_enabled(self.dynamic_input)
 
-    def disable_input(self):
-        self.disable_btn.click()
-        return self.dynamic_input.is_disabled()
+    def disable_input(self) -> bool:
+        self.click(self.disable_btn)
+        return self.wait_disabled(self.dynamic_input)
 
-    def fill_input(self, text: str):
-        self.dynamic_input.fill(text)
-        return self.dynamic_input.input_value()
+    def fill_input(self, text: str = "Playwright dynamic controls") -> str:
+        self.fill(self.dynamic_input, text)
+        self.expect_value(self.dynamic_input, text)  # <-- ważne!
+        return text
 
-    def get_toast_message(self):
-        return self.toast.inner_text()
+    def get_toast_message(self) -> str:
+        self.wait_visible(self.toast)
+        return self.toast.inner_text().strip()
 
     def sort_last_name_toggle(self):
-        self.c_headers.nth(0).click()
+        self.click(self.c_headers.nth(0))
 
     def get_last_name_values(self) -> list[str]:
-        count = self.last_name_cells.count()
-        values = []
+        return self.get_visible_texts(self.last_name_cells)
 
-        for i in range(count):
-            cell = self.last_name_cells.nth(i)
-            if cell.is_visible():
-                values.append(cell.text_content().strip())
-
-        return values
 
 
 
