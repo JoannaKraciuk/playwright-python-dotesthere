5295fa0dde3446091d6075a6c1887bc426237813
Joanna Kraciuk <j.kraciuktest@gmail.com>
2026-01-21 15:26:18 +0100

    Dodanie testów błędnego logowania

--- tests/test_login.py
+++ tests/test_login.py
@@ -1,12 +1,13 @@
 import pytest
 import allure
 from pages.login_page import LoginPage
+from utils.test_data import USERNAMES, PASSWORDS
 
 @allure.epic("Strona główna")
-@allure.feature("Weryfikacja adresu url")
+@allure.feature("Testy url oraz logowania")
 @allure.severity(allure.severity_level.CRITICAL)
 class TestLogin:
-    @allure.story("Użytkownik po przejściu do strony https://dotesthere.com/ weryfikuje jej aktualny adres url")
+    @allure.story("Użytkownik po przejściu na stronę główną, weryfikuje jej aktualny adres url")
     def test_open_login(self, login_page: LoginPage):
 
         with allure.step("Przejście do strony https://dotesthere.com/"):
@@ -15,14 +16,51 @@ class TestLogin:
         with allure.step("Weryfikacja prawidłowego adresu url strony."):
             assert actual_url == 'https://dotesthere.com/', "Adres strony jest nieprawidłowy"
 
+    @pytest.mark.parametrize(("username", "password"), [(USERNAMES["valid"], PASSWORDS["valid"])])
     @allure.story("Użytkownik może zalogować się poprawnymi danymi")
-    def test_login(self, login_page: LoginPage):
+    def test_login(self, login_page: LoginPage, username, password):
 
         with allure.step("Wejście na stronę logowania"):
             login_page.open()
 
         with allure.step("Wprowadzenie poprawnych danych"):
-            alert_message = login_page.login('ankur', 'automation')
+            alert_message = login_page.login(username, password)
 
         with allure.step("Weryfikacja komunikatu i widoku po zalogowaniu"):
             assert alert_message == 'You logged into a secure area!', "Brak komunikatu powitalnego"
+
+        with allure.step("Weryfikacja komunikatu sukcesu"):
+            toast_message = login_page.read_toast()
+            assert toast_message == 'Login successful!'
+
+    @pytest.mark.parametrize(("username", "password"), [(USERNAMES["invalid"], PASSWORDS["valid"])])
+    @allure.story("Próba logowania błędnym loginem")
+    def test_login_invalid_username(self, login_page: LoginPage, username, password):
+        with allure.step("Wejście na stronę logowania"):
+            login_page.open()
+
+        with allure.step("Wprowadzenie niepoprawnego loginu i poprawnego hasła"):
+            alert_message = login_page.login(username, password)
+
+        with allure.step("Weryfikacja komunikatu i widoku po zalogowaniu"):
+            assert alert_message == "Your username is invalid!", "Brak komunikatu o błędzie"
+
+        with allure.step("Weryfikacja komunikatu sukcesu"):
+            toast_message = login_page.read_toast()
+            assert toast_message == 'Invalid credentials'
+
+    @pytest.mark.parametrize(("username", "password"), [(USERNAMES["valid"], PASSWORDS["invalid"])])
+    @allure.story("Próba logowania błędnym hasłem")
+    def test_login_invalid_password(self, login_page: LoginPage, username, password):
+        with allure.step("Wejście na stronę logowania"):
+            login_page.open()
+
+        with allure.step("Wprowadzenie poprawny login i niepoprawne hasło"):
+            alert_message = login_page.login(username, password)
+
+        with allure.step("Weryfikacja komunikatu i widoku po zalogowaniu"):
+            assert alert_message == "Your username is invalid!", "Brak komunikatu o błędzie"
+
+        with allure.step("Weryfikacja komunikatu sukcesu"):
+            toast_message = login_page.read_toast()
+            assert toast_message == 'Invalid credentials'
--- utils/test_data.py
+++ utils/test_data.py
@@ -7,3 +7,5 @@ config = configparser.ConfigParser()
 config.read(ROOT_DIR / "config" / "test_data.ini")
 
 UPLOAD_FILES = config["upload_files"]
+USERNAMES = dict(config["username"])
+PASSWORDS = dict(config["password"])
